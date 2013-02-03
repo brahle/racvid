4bb22a01204a4d945275841c2669d14e8d791b1e
Bruno Rahle <brahle@gmail.com>
2013-02-03 17:09:15 +0100

    HOG detector
    Detektor ljudi - jako je spor.

--- background.cpp
+++ background.cpp
@@ -46,6 +46,7 @@ int main(int argc, char* argv[]) {
   cvMoveWindow("B", 800, 10);
 
   MyBackgroundSubtractor bg;
+  HOGDescriptor hog;
 
   vector<Mat> frames;
   Mat background;
@@ -71,8 +72,34 @@ int main(int argc, char* argv[]) {
         if (!foregroundMask.at<char>(i, j))
           frame.at<Vec3b>(i, j) = 0;
 
-    imshow("A", orig);
-    imshow("B", frame);
+    Mat img = orig.clone();
+    vector<Rect> found, found_filtered;
+    hog.detectMultiScale(img, found, 0, Size(), Size(), 1.05, 2);
+    hog.setSVMDetector(HOGDescriptor::getDefaultPeopleDetector());
+
+    size_t i, j;
+    for (i=0; i<found.size(); i++) {
+      Rect r = found[i];
+      cout << r << endl;
+       for (j=0; j<found.size(); j++)
+        if (j!=i && (r & found[j])==r)
+          break;
+        if (j==found.size())
+          found_filtered.push_back(r);
+    }
+    for (i=0; i<found_filtered.size(); i++) {
+      Rect r = found_filtered[i];
+      r.x += cvRound(r.width*0.1);
+	    r.width = cvRound(r.width*0.8);
+	    r.y += cvRound(r.height*0.06);
+	    r.height = cvRound(r.height*0.9);
+	    rectangle(img, r.tl(), r.br(), cv::Scalar(0,255,0), 2);
+    }
+    imshow("video capture", img);
+
+//    imshow("A", orig);
+//    imshow("B", frame);
+    imshow("C", foregroundMask);
     //    imshow("back", background);
 
     if (waitKey(30) == 27) break;
